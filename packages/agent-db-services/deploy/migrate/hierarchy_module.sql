98d5fa0768d625e1d0519dc5ea64c94512f4fd36
Dan Lynch <pyramation@gmail.com>
2026-03-15 14:45:29 -0700

    fix(services): correct hardcoded schema names in SQL migrations to use agent_db_ prefix instead of agent_db_services_

--- packages/agent-db-services/deploy/migrate/hierarchy_module.sql
+++ packages/agent-db-services/deploy/migrate/hierarchy_module.sql
@@ -41,7 +41,7 @@ INSERT INTO metaschema_modules_public.hierarchy_module (
   get_managers_function,
   is_manager_of_function
 ) VALUES
-  ('64fd98ec-4b1d-4ac8-8719-4658501fd219', '9085366f-6c6f-49b7-a4c9-c82d816fded5', 'a0087970-3d1f-473c-4c81-937a8d8eeb0c', 'a008cef5-a8b2-4e8e-a661-699d408148cc', 'a008fa84-9155-47af-551f-5c7448fc2b82', 'org_chart_edges', 'a008b925-c195-45fe-45eb-ec1a314c2018', 'org_hierarchy_sprts', 'a008cdc0-3d5c-4119-b02e-1f67916ea037', 'org_chart_edge_grants', 'a0082485-cf01-4c72-8c4f-76422cceded1', 'a0082485-cf01-4c72-8c4f-76422cceded1', 'org', 'agent_db_services_memberships_private', 'org_hierarchy_sprts', 'org_rebuild_org_hierarchy_sprt', 'org_get_subordinates', 'org_get_managers', 'org_is_manager_of');
+  ('64fd98ec-4b1d-4ac8-8719-4658501fd219', '9085366f-6c6f-49b7-a4c9-c82d816fded5', 'a0087970-3d1f-473c-4c81-937a8d8eeb0c', 'a008cef5-a8b2-4e8e-a661-699d408148cc', 'a008fa84-9155-47af-551f-5c7448fc2b82', 'org_chart_edges', 'a008b925-c195-45fe-45eb-ec1a314c2018', 'org_hierarchy_sprts', 'a008cdc0-3d5c-4119-b02e-1f67916ea037', 'org_chart_edge_grants', 'a0082485-cf01-4c72-8c4f-76422cceded1', 'a0082485-cf01-4c72-8c4f-76422cceded1', 'org', 'agent_db_memberships_private', 'org_hierarchy_sprts', 'org_rebuild_org_hierarchy_sprt', 'org_get_subordinates', 'org_get_managers', 'org_is_manager_of');
 
 
 SET session_replication_role TO DEFAULT;
